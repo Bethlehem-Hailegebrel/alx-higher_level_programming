23f8a55497d85d6a23f61276cb0679be1c17ae82
Bethlehem-Hailegebrel <kiyakeys7@gmail.com>
2024-03-25 08:11:12 -0700

    updated task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-'''a program that prints the ASCII alphabet, in lowercase, not followed by a new line'''
+# a program that prints the ASCII alphabet
 for i in range(97, 123):
     if (i == 101) or (i == 113):
         continue
     print("{}".format(chr(i)), end="")
-
